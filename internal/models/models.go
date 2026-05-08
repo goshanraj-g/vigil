eff77e93bf54898db5c094718da8cf1b07ec7f58
Goshanraj Govindaraj <govindag@mcmaster.ca>
2026-05-08 00:21:39 -0400

    init: db model

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,1 +1,22 @@
 package models
+
+import "time"
+
+type Monitor struct {
+	ID             string `json:"id"`
+	APIKeyHash     string `json:"-"`
+	Query          string `json:"query"`
+	WebhookURL     string `json:"webhook_url"`
+	Schedule       string `json:"schedule"`
+	Status         string `json:"status"`
+	CreatedAt      time.Time `json:"created_at"`
+}
+
+type Result struct {
+	ID            string `json:"id"`
+	MonitorID     string `json:"monitor_id"`
+	URL           string `json:"url"`
+	Title         string `json:"title"`
+	Snippet       string `json:"snippet"`
+	FoundAt       time.Time `json:"foundAt"`
+}
